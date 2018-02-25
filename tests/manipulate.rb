5faeb334d7c80561bf7e2ea1f58d4c849cf5c342
yangchuanosaurus <355592261@qq.com>
2018-02-26 00:57:04 +0800

    Improve manipulate functions

--- tests/manipulate.rb
+++ tests/manipulate.rb
@@ -1,38 +1,48 @@
 require 'json'
 
-def manipulate_hash(data_hash)
-	p "manipulate hash"
-	data_hash.map do |key, value|
-		puts "#{key} : #{value.class}"
+def manipulate_hash(name, data_hash)
+	#p "manipulate hash of #{name}.model"
+	data_hash.keys.each do |key|
+		value = data_hash[key]
 		if value.is_a?(Hash)
-			p "#{key} mapping"
-			manipulate_hash(value)
+			puts "#{name}.model #{key} : #{key}.model"
 		elsif value.is_a?(Array)
-			manipulate_array(value)
+			puts "#{name}.model #{key} : #{key}Array<#{key}Item>.model"
+		else
+			puts "#{name}.model #{key} : #{value.class}"
+		end
+	end
+
+	data_hash.map do |key, value|
+		if value.is_a?(Hash)
+			manipulate_hash(key, value)
+		elsif value.is_a?(Array)
+			manipulate_array("#{key}Item", value)
 		end
 	end
 end
 
-def manipulate_array(data_array)
-	p "manipulate array"
+def manipulate_array(name, data_array)
+	#p "manipulate array of #{name}.model"
 	data_array.each do |value|
 		
 		if value.is_a?(Hash)
-			manipulate_hash(value)
+			manipulate_hash(name, value)
 		elsif value.is_a?(Array)
-			manipulate_array(value)
+			manipulate_array(name, value)
 		else
-			p "directly mapping as #{value} : #{value.class}"
+			p "#{name}.model : #{value.class}"
 		end
-		
+
 	end
 end
 
-def manipulate(data_json)
+def manipulate(name, data_json)
+	p "manipulate #{name}.model"
 	if data_json.is_a?(Hash)
-		manipulate_hash(data_json)
+		manipulate_hash(name, data_json)
 	elsif data_json.is_a?(Array)
-		manipulate_array(data_json)
+		manipulate_array("#{name}Item", data_json)
 	end
 end
 
@@ -45,7 +55,18 @@ json = %Q{{
 	"success": true
 }}
 
-p json.class
+json_array = %Q{{
+		"results": [
+			{
+				"a": 1, 
+				"b": true, 
+				"c": 1.0, 
+				"d": "d"
+			}
+		]
+	}
+}
 
-resp_body = JSON.parse(json)
-manipulate(resp_body)
+resp_body = JSON.parse(json_array)
+
+manipulate('root', resp_body)
